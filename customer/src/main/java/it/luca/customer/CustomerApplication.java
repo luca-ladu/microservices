7d78670515dd859f686a88e7c4baf359f6e579cb
LucaLadu <laduluca78@gmail.com>
2022-04-07 09:34:15 +0200

    Ora funziona

--- customer/src/main/java/it/luca/customer/CustomerApplication.java
+++ customer/src/main/java/it/luca/customer/CustomerApplication.java
@@ -1,8 +1,10 @@
 package it.luca.customer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class,args);
--- customer/src/main/java/it/luca/customer/model/Customer.java
+++ customer/src/main/java/it/luca/customer/model/Customer.java
@@ -1,20 +1,28 @@
 package it.luca.customer.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String firstName;
     private String lastName;
